b45fa37e068fd0f792d2752eeedadb204c78057e
Nzar <nzar.r.ahmed@gmail.com>
2022-02-19 16:07:45 +0000

    Initial Project Commit

---
+++ KurdishCalendarExtensionMethods.cs
@@ -0,0 +1,103 @@
+﻿namespace KurdishCalendar
+{
+    public static class KurdishCalendarExtensionMethods
+    {
+
+        internal static string[] kurdishMonth = { "نەورۆز", "گوڵان", "جۆزەردان", "پوشپەڕ", "گەلاوێژ", "خەرمانان","ڕەزبەر","گەڵاڕێزان","سەرماوەرز","بەفرانبار","ڕێبەندان","ڕەشيەمێ" };
+        internal static string[] kurdishDayOfWeek = { "شەممە", "یەکشەممە", "دووشەممە", "سێ شەممە", "چوارشەممە", "پێنج شەممە", "هەینی" };
+        
+        public static string toKurdish(this DateTime dateTime)
+        {
+            Boolean IS_FIRST_YEAR_LEAP = false;
+            Boolean IS_LAST_YEAR_LEAP = false;
+            int TOTAL_DAY_OF_YEAR = 0;
+            if(dateTime<=new DateTime(dateTime.Year,3,21))
+            {
+
+            }
+            else
+            {
+
+            }
+            return "";
+        }
+        public static string toKurdishDate(this DateTime dateTime)
+        {
+            DateTime __KurdishDateTime = dateTime.AddDays(255590);
+            return $"{__KurdishDateTime.Year}-{__KurdishDateTime.Month}-{__KurdishDateTime.Day}";
+        }
+        public static string toKurdishDateVerbose(this DateTime datetime)
+        {
+            DateTime __kurdishDateTime = datetime.AddDays(255590);
+            return $"{__kurdishDateTime.Year}-{kurdishMonth[__kurdishDateTime.Month-1]}-{__kurdishDateTime.Day}";
+        }
+        public static Boolean isLeapYear(this DateTime dateTime)
+        {
+            try
+            {
+                if (dateTime.Year%400==0 || (dateTime.Year%4==0 && dateTime.Year%100!=0))
+                    return true;
+                return false;
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+        public static string MapToKurdish(this DateTime datetime)
+        {
+            DateTime FIRST_DAY_OF_KURDISH_YEAR;
+            int KURDISH_YEAR_TOTAL_DAYS = 0;
+           if(datetime>=new DateTime(datetime.Year, 3, 21))
+            {
+                FIRST_DAY_OF_KURDISH_YEAR=new DateTime(datetime.Year, 3, 21);
+            }else
+            {
+                FIRST_DAY_OF_KURDISH_YEAR=new DateTime(datetime.Year-1, 3, 21);
+            }
+            KURDISH_YEAR_TOTAL_DAYS=(datetime-FIRST_DAY_OF_KURDISH_YEAR).Days+1;
+            return new KurdishCalendar.KuridshDateTime(KURDISH_YEAR_TOTAL_DAYS).ToString();
+        }
+        public static int getTotalKurdishDay(this DateTime datetime)
+        {
+            DateTime FIRST_DAY_OF_KURDISH_YEAR;
+            int KURDISH_YEAR_TOTAL_DAYS = 0;
+            if (datetime>=new DateTime(datetime.Year, 3, 21))
+            {
+                FIRST_DAY_OF_KURDISH_YEAR=new DateTime(datetime.Year, 3, 21);
+            }
+            else
+            {
+                FIRST_DAY_OF_KURDISH_YEAR=new DateTime(datetime.Year-1, 3, 21);
+            }
+            KURDISH_YEAR_TOTAL_DAYS=(datetime-FIRST_DAY_OF_KURDISH_YEAR).Days+1;
+            return KURDISH_YEAR_TOTAL_DAYS;
+        }
+        public static DateTime getFirstKurdishDateStart(this DateTime datetime)
+        {
+            DateTime FIRST_DAY_OF_KURDISH_YEAR;
+            if (datetime>=new DateTime(datetime.Year, 3, 21))
+            {
+                FIRST_DAY_OF_KURDISH_YEAR=new DateTime(datetime.Year, 3, 21);
+            }
+            else
+            {
+                FIRST_DAY_OF_KURDISH_YEAR=new DateTime(datetime.Year-1, 3, 21);
+            }
+            return FIRST_DAY_OF_KURDISH_YEAR;
+        }
+        public static int mapKurdishYear(this DateTime datetime)
+        {
+            DateTime FIRST_DAY_OF_KURDISH_YEAR;
+            if (datetime>=new DateTime(datetime.Year, 3, 21))
+            {
+                FIRST_DAY_OF_KURDISH_YEAR=new DateTime(datetime.Year, 3, 21);
+            }
+            else
+            {
+                FIRST_DAY_OF_KURDISH_YEAR=new DateTime(datetime.Year-1, 3, 21);
+            }
+            return FIRST_DAY_OF_KURDISH_YEAR.Year+700;
+        }
+    }
+}
---
+++ KuridshDateTime.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KurdishCalendar
+{
+    public class KuridshDateTime
+    {
+
+        private  string[] KURDISH_MONTHS = { "نەورۆز","گوڵان","جۆزەردان","پوشپەڕ","گەڵاوێژ","خەرمانان","ڕەزبەر","گەڵاڕێزان","سەرماوەرز","بەفرانبار","ڕێبەندان","ڕەشەمێ"}; 
+        private int day { set; get; }
+        private int month { set; get; }
+        private int year { set; get; }
+
+        private Func<int,int,int, (int,int,int)> CALCULATE_DAY_PER_YEAR =(x,y,z) =>{
+            int year = 1;
+            int month = (int)Math.Floor((decimal)(x-1)/y)+1;
+            int days= x%y;
+            days=days==0?y: days;
+            return (year,month+z,days);
+        };
+        private Func<int,int,int, int, (int,int, int)> CALCULATE_DAY_PER_YEAR_TOTAL = (year,x,y,z) => {
+            int month = (int)Math.Floor((decimal)(x-1)/y)+1;
+            int days = x%y;
+            days=days==0 ? y : days;
+            return (year,month+z, days);
+        };
+
+        public KuridshDateTime(int totaldays) => (this.year, this.month, this.day)= totaldays switch
+        {
+            var x when x<=186 => CALCULATE_DAY_PER_YEAR(x, 31, 0),
+            var x when x>186 => CALCULATE_DAY_PER_YEAR(x-186, 30, 6),
+            _ => (1, 1, 1)
+        };
+        public KuridshDateTime(DateTime datetime) => (this.year, this.month, this.day)=datetime.getTotalKurdishDay() switch
+        {
+            var x when x<=186 => CALCULATE_DAY_PER_YEAR_TOTAL(datetime.mapKurdishYear(),x, 31, 0),
+            var x when x>186 => CALCULATE_DAY_PER_YEAR_TOTAL(datetime.mapKurdishYear(),x-186, 30, 6),
+            _ => (1, 1, 1)
+        };
+
+       
+        public override string ToString() => $"{this.year} - {KURDISH_MONTHS[this.month-1]} - {this.day} ";
+        
+    }
+}
